b6e848fee359af117794dbcde6d7e596c3c4e56e
Ripal Patel <ripal212007@gmail.com>
2018-09-22 15:46:05 -0500

    Added function to decode

--- main.js
+++ main.js
@@ -16,23 +16,21 @@ const firstStringBuilder = () => {
 // EventListener for convert to secret button
 const secretButton = document.getElementById('secret-button');
 secretButton.addEventListener ('click', (e) => {
-    // e.stopPropagation();
     firstStringBuilder();
 })
 
 // Function for convert to words
 const secondStringBuilder = () => {
     let inputString = document.getElementById('user2-input').value;
-    let inputStringArray = inputString.split('');
+    let inputStringArray = inputString.split(',');
     for (let i = 0; i <inputStringArray.length; i++) {
-         outputString = inputStringArray[i].string.fromCharCode();   
+        let outputString =String.fromCharCode(inputStringArray[i]);   
         printToDom(outputString, 'secondOutput');
     }
 }
 // EventListener for convert to word button
 const wordButton = document.getElementById('word-button');
-wordButton.addEventListener = ('click', (e) => {
-    e.preventDefault();
-
+wordButton.addEventListener ('click', () => {
+    secondStringBuilder();
 })
 
